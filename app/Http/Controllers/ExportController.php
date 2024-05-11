146f3df51a0e180d91b86d0cd84ab1a3a867fae8
HanaDewi <hanadewi@students.unnes.ac.id>
2024-05-11 16:06:24 +0700

    pdf export

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -2,43 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use Barryvdh\DomPDF\PDF;
+use Barryvdh\DomPDF\Facade as PDF;
 use App\Models\Pencapaian;
 use Illuminate\Http\Request;
 use App\Exports\PencapaianExport;
 use App\Http\Controllers\Controller;
 use Maatwebsite\Excel\Facades\Excel;
 
+
+
 class ExportController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        $tahun = Pencapaian::selectRaw('tahun')->distinct()->get();
-        $apbd = Pencapaian::select('apbd')->distinct()->get();
-        $keg = Pencapaian::select('keg')->distinct()->get();
-        return view("export.index",compact("tahun","apbd","keg"));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
+    // Your other methods...
 
     /**
      * Store a newly created resource in storage.
      */
     public function export(Request $request)
     {
-        $pencapaians = Pencapaian::where("tahun",$request->tahun)->where("keg",$request->keg)->where("apbd",$request->apbd)->get();
+        $pencapaians = Pencapaian::where("tahun", $request->tahun)
+            ->where("keg", $request->keg)
+            ->where("apbd", $request->apbd)
+            ->get();
+
         if ($request->export == "pdf") {
-            $pdf = \PDF::loadView('export.pencapaian_pdf',["pencapaians" => $pencapaians]);
+            $pdf = PDF::loadView('export.pencapaian_pdf', ["pencapaians" => $pencapaians]);
             $pdf->setPaper('A4', 'landscape');
             return $pdf->stream();
-        } else if($request->export == "excel"){
+        } elseif ($request->export == "excel") {
             return Excel::download(new PencapaianExport($request), 'pencapaian.xlsx');
         }
-
     }
 }
--- config/app.php
+++ config/app.php
@@ -159,7 +159,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+    
         /*
          * Application Service Providers...
          */
@@ -168,7 +168,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
     ])->toArray(),
+    
 
     /*
     |--------------------------------------------------------------------------
